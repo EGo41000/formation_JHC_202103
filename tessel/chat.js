7878e1eccfa29339455be1ae2a8b1877fbe231c6
Emmanuel Goudot <Emmanuel.Goudot@gmail.com>
2021-03-23 12:37:26 +0100

    Ajout de la lecture de carte RFID

--- tessel/chat.js
+++ tessel/chat.js
@@ -1,8 +1,14 @@
 // SERVEUR
 
+var tessel = require('tessel');
+var rfidlib = require('rfid-pn532');
+
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+
+var rfid = rfidlib.use(tessel.port['A']);
+
 
 app.get('/:fn?', (req, res) => {
   console.log("app GET "+req.params.fn);
@@ -12,6 +18,17 @@ app.get('/:fn?', (req, res) => {
   //res.sendFile(__dirname + '/index.html');
   res.sendFile(__dirname + '/'+req.params.fn);
 });
+
+rfid.on('ready', function (version) {
+  console.log('Ready to read RFID card, version=', version);
+
+  rfid.on('data', function(card) {
+    console.log(card);
+    console.log('UID:', card.uid.toString('hex'));
+    io.emit('chat message', "RFID cart: "+card.uid.toString('hex'));
+  });
+});
+
 
 io.on('connection', (socket) => {
   console.log('user ['+socket.id+'] connected @ '+socket.handshake.address);
@@ -40,3 +57,8 @@ io.on('connection', (socket) => {
 http.listen(3000, () => {
   console.log('listening on *:3000');
 });
+
+rfid.on('error', function (err) {
+  console.error(err);
+});
+
